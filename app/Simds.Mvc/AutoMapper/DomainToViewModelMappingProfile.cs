e26d899a80da0b63658daa8691453f65eb57a279
iniciativad <iniciativad@gmail.com>
2015-03-04 17:46:50 -0300

    Configuração AutoMapper

---
+++ app/Simds.Mvc/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+
+namespace Simds.Mvc.AutoMapper
+{
+	public class DomainToViewModelMappingProfile : Profile
+	{
+		public override string ProfileName
+		{
+			get { return "ViewModelToDomainMappingProfile"; }
+		}
+
+		protected override void Configure()
+		{
+			Mapper.CreateMap<ClienteViewModel, Cliente>();
+			Mapper.CreateMap<ProdutoViewModel, Produto>();
+		}
+	}
+}
---
+++ app/Simds.Mvc/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+
+namespace Simds.Mvc.AutoMapper
+{
+	public class ViewModelToDomainMappingProfile :Profile
+	{
+		public override string ProfileName
+		{
+			get { return "DomainToViewModelMappingProfile"; }
+		}
+
+		protected override void Configure()
+		{
+			Mapper.CreateMap<Cliente, ClienteViewModel>();
+			Mapper.CreateMap<Produto, ProdutoViewModel>();
+		}
+	}
+}
